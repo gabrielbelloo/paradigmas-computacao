c3162f2d5180cdd0b3d08eb27e18e8a8972ac1ff
Gabriel Bello <bellodealmeidagabriel@gmail.com>
2026-09-15 20:06:37 -0300

    feat: add aula004 exercises

---
+++ aula004/Programas_Java_Capitulo_4/03-diagnosticos/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    static final String ALUNO = "GABRIEL_BELLO_DE_ALMEIDA";
+    static final String RA = "240331642";
+
+    public static void main(String[] args) {
+        System.out.println("ALUNO: " + ALUNO + " | RA: " + RA);
+
+        int total = 10 + 20; // CORRIGIR 1: acrescente o ponto e virgula
+
+        if (total > 20) { // CORRIGIR 2: feche corretamente o parenteses
+            System.out.println("Programa corrigido!"); // CORRIGIR 3: feche o texto entre aspas
+            System.out.println("Resultado: " + total);
+        }
+    }
+}
